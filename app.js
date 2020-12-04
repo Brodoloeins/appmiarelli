d9284220ac1ca2e46372238622153d2cd746db96
Brodoloeins <72166551+Brodoloeins@users.noreply.github.com>
2020-12-03 21:27:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,562 @@
+//Campo de variáveis e constantes
+var res = 0;
+var opem = 0;
+var open1 = 0;
+var open2 = 0;
+var open3 = 0;
+var open4 = 0;
+var open5 = 0;
+var open6 = 0;
+var open7 = 0;
+var open8 = 0;
+var open9 = 0;
+var open10 = 0;
+var open11 = 0;
+var open12 = 0;
+
+//Campo de function
+
+//Fuction de redirecionar para o mapa mental
+
+function redirect(){
+    window.location.href="https://mm.tt/1689650276?t=BnTBwkuNaa"
+}
+
+//Function de redirecionar para as perguntas
+
+function redi(){
+    window.location.href="./perguntas.html"
+}
+
+//Function da primeira pergunta
+function op11 (){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem1;
+    if(opem >= 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem1 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op12 (){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem1;
+    if(opem >= 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem1 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da segunda pergunta
+
+function op21(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem2;
+    if(opem >= 2){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem2 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op22(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem2;
+    if(opem >= 2){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem2 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op23(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem2;
+    if(opem >= 2){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem2 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op24(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem2;
+    if(opem >= 2){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem2 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da terceira pergunta
+
+function op31(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem3;
+    if(opem >= 3){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem3 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+} 
+function op32(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem3;
+    if(opem >= 3){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem3 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op33(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem3;
+    if(opem >= 3){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem3 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da quarta pergunta
+
+function op41(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem4;
+    if(opem >= 4){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem4 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+
+function op42(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem4;
+    if(opem >= 4){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem4 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+
+function op43(){
+    alert("Você errou")
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem4;
+    if(opem >= 4){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem4 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+
+//Function da quinta pergunta
+
+function op51(){
+    alert("Você errou")
+    
+    ++opem
+    ++opem5;
+    if(opem >= 5){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem5 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+function op52() {
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem
+    ++opem5;
+    if(opem >= 5){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem5 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+function op53(){
+    alert("Você errou")
+    
+    ++opem
+    ++opem5;
+    if(opem >= 5){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if( opem5 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    } 
+}
+
+//Function da sexta pergunta
+
+function op61(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem6;
+    if(opem >= 6){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem6 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op62(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem6;
+    if(opem >= 6){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem6 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op63(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem6;
+    if(opem >= 6){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem6 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da sétima pergunta
+
+function op71(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem7;
+    if(opem >= 7){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem7 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op72(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem7;
+    if(opem >= 7){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem7 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op73(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem7;
+    if(opem >= 7){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem7 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da oitava pergunta
+
+function op81(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem8;
+    if(opem >= 8){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem8 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op82(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem8;
+    if(opem >= 8){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem8 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op83(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem8;
+    if(opem >= 8){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem8 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+
+//Function da nona pergunta
+
+function op91(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem9;
+    if(opem >= 9){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem9 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op92(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem9;
+    if(opem >= 9){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem9 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op93(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem9;
+    if(opem >= 9){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem9 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Function da décima pergunta
+
+function op101(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem10;
+    if(opem >= 10){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem10 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op102(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem10;
+    if(opem >= 10){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem10 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op103(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem10;
+    if(opem >= 10){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem10 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op104(){
+    alert("você acertou");
+    ++res;
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem10;
+    if(opem >= 10){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem10 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+function op105(){
+    alert("você errou");
+
+    //Campo da conferencia de legitimedade
+
+    ++opem;
+    ++opem10;
+    if(opem >= 10){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+    if(opem10 > 1){
+        window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=179"
+    }
+}
+
+//Campo de if e switch
+
+//Conferência da pontuação e vericidade dela
+
+//Conferência da veracidade
+
+if(opem == 10 && res>10){
+    window.location.href = "https://meet.google.com/dms-bybd-uav?authuser=0&hs=17"
+}
+
+//Usando o else e if para ser mais rápida a conferência do resultado
+//Uso também o && para verificar se foram apertadas 5 tipos de questões
+
+else if(opem == 10 && res == 10){
+    alert("Parabéns o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res == 9){
+    alert("Foi quase, mas não foi dessa vez, o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res == 8){
+    alert("Foi quase, mas não foi dessa vez o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res ==7){
+    alert("Foi quase, mas não foi dessa vez o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res ==6){
+    alert("Foi quase, mas não foi dessa vez o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res ==5){
+    alert("Você acertou "+ res )
+}
+else if(opem == 10 && res ==4){
+    alert("Estude mais um pouco a matéria, o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res ==3){
+    alert("Estude mais um pouco a matéria, o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res == 2){
+    alert("Estude mais um pouco a matéria, o seu resultado foi de " + res + " acertos")
+}
+else if(opem == 10 && res == 1){
+    alert("Estude mais um pouco a matéria, o seu resultado foi de " + res + " acertos")    
+}
+else if(opem == 10 && res == 0){
+    alert("Você foi muito mal, não prestou atenção na explcação e nem estudou a matéria, o seu resultado foi de " + res + " acertos")
+}
